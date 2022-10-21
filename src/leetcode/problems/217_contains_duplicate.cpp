958a6473720b15afff83d74f1d59072147f4724e
Supreeth Kumar K <skk467@nyu.edu>
2022-10-21 01:23:01 -0700

    added 217 problem's logic 1

--- src/leetcode/problems/217_contains_duplicate.cpp
+++ src/leetcode/problems/217_contains_duplicate.cpp
@@ -38,24 +38,67 @@ using std::unique;
 
 class Solution {
   public:
-    void printVector(vector<int> v) {
-      for (int i : v)
-        std::cout << i << ' ';
-      std::cout << '\n';
+    /**
+     * @brief Function to test all the vectors and show
+     *        the result on the terminal output
+     * 
+     * @param n Testable integer vector
+     */
+    void testVector(vector<int> &n) {
+      cout << "Vector = ";
+      printVector(n);
+      if (containsDuplicate(n) == 1) 
+        cout << "Yes, vector consists of duplicate values!" << endl;
+      else
+        cout << "No, vector does not consists of duplicate values!" << endl;  
+      cout << endl;
+
+      return;
     }
 
+    /**
+     * @brief Print vector type onto the terminal output
+     * 
+     * @param v Printable integer vector
+     */
+    void printVector(vector<int> v) {
+      for (int i : v)
+        cout << i << ' ';
+      cout << endl;
+
+      return;
+    }
+
+    /**
+     * @brief Logic to check if the given integer vector consists of 
+     *        any duplicate integer values
+     * 
+     * @param nums Given integer vector
+     * @return true 
+     * @return false 
+     */
     bool containsDuplicate(vector<int> &nums) {
-      // TODO: add logic 
-      printVector(v);
-      return 0;
+      for (int i=0; i<nums.size(); ++i) {
+        for (int j=0; j<i; ++j) {
+          if (nums[j] == nums[i])
+            return true;
+        }
+      }
+      
+      return false;
     }
 };
 
 int main() {
   Solution sol;
-  vector<int> nums {1,2,3,4};
-  
-  cout << sol.containsDuplicate(nums) << endl;
 
+  vector<int> nums {1,2,3,1};
+  vector<int> nums1 {1,2,3,4};
+  vector<int> nums2 {1,1,1,3,3,4,3,2,4,2}; 
+
+  sol.testVector(nums);
+  sol.testVector(nums1);
+  sol.testVector(nums2);
+  
   return 0;
 }
