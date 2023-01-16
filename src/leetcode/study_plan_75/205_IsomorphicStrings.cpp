27563e7343cca91d41ce8adfa3886ed13c994c33
Supreeth Kumar K <skk467@nyu.edu>
2023-01-15 23:49:01 -0800

    added 205 isomorphic strings question

---
+++ src/leetcode/study_plan_75/205_IsomorphicStrings.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file 205_IsomorphicStrings.cpp
+ * @author skk
+ * @brief 
+ * Given two strings s and t, determine if they are isomorphic.
+ * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+ * All occurrences of a character must be replaced with another character while 
+ * the order of characters. No two characters may map to the same character, but a 
+ * character may map to itself.
+ * 
+ * Example 1:
+ * Input: s = "egg", t = "add"
+ * Output: true
+ * 
+ * Example 2:
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ * 
+ * Example 3:
+ * Input: s = "paper", t = "title"
+ * Output: true
+ * 
+ * Constraints:
+ * 1 <= s.length <= 5 * 104
+ * t.length == s.length
+ * s and t consist of any valid ascii character.
+ * 
+ * @version 0.1
+ * @date 2023-01-15
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
